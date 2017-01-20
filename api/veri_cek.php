d4e69c5847870a36152ed6bec774b7523644ee92
Emrah Gümüş <emrahgumus34@gmail.com>
2017-01-20 12:14:39 +0300

    kullanıma hazır

--- api/veri_cek.php
+++ api/veri_cek.php
@@ -13,7 +13,7 @@ switch ($_REQUEST['goster']){
         $t1 = $_REQUEST['t1'];
         $t2 = $_REQUEST['t2'];
 
-        $qry = $db->query("SELECT akim, gerilim, sicaklik, nem, DATE_FORMAT(tarih, '%m-%d-%Y %H:%i:%s') AS tarih 
+        $qry = $db->query("SELECT akim, gerilim, sicaklik, nem, DATE_FORMAT(tarih, '%m-%d-%Y %H:%i:%s') AS tarih
                   FROM veriler WHERE (tarih BETWEEN '".$t1."' AND '".$t2."') ORDER BY id DESC LIMIT 200");
 
         $arr = array();
@@ -28,11 +28,10 @@ switch ($_REQUEST['goster']){
 
     default:
 
-        #date('Y-m-d H:i:s', time() - 60); //1 dk önce
-
+        /*
         $tarih = date('Y-m-d H:i:s', time() - 60);
 
-        $qry = $db->query("SELECT akim, gerilim, sicaklik, nem, DATE_FORMAT(tarih, '%m-%d-%Y %H:%i:%s') AS tarih 
+        $qry = $db->query("SELECT akim, gerilim, sicaklik, nem, DATE_FORMAT(tarih, '%m-%d-%Y %H:%i:%s') AS tarih
                   FROM veriler WHERE tarih >= '".$tarih."' ORDER BY id DESC LIMIT 200");
 
         $arr = array();
@@ -42,9 +41,33 @@ switch ($_REQUEST['goster']){
         }
 
         echo json_encode($arr);
+*/
+
+
+        $qry = $db->query("SELECT akim, gerilim, sicaklik, nem, DATE_FORMAT(tarih, '%m-%d-%Y %H:%i:%s') AS tarih
+                          FROM veriler ORDER BY id DESC LIMIT 200");
+
+        $arr = null;
+
+        $arr[0]['name'] = 'istanbul';
+        $arr[1]['name'] = 'ankara';
+        $arr[2]['name'] = 'izmir';
+        $arr[3]['name'] = 'kocaeli';
+
+
+        while ($row = $qry->fetch_assoc()){
+
+          $arr[0]['data'][] = $row['akim'];
+          $arr[1]['data'][] = $row['akim']+rand(5, 15);
+          $arr[2]['data'][] = $row['akim']+rand(5, 15);
+          $arr[3]['data'][] = $row['akim']+rand(5, 15);
+
+        }
+
+        echo json_encode($arr);
+
+
 
         break;
 
 }
-
-
--- api/veri_kaydet.php
+++ api/veri_kaydet.php
@@ -4,6 +4,7 @@
 
 include './sys/db.php';
 
+$panel      = $_REQUEST['panel'];
 $akim       = $_REQUEST['akim'];
 $gerilim    = $_REQUEST['gerilim'];
 $sicaklik   = $_REQUEST['sicaklik'];
@@ -11,8 +12,8 @@ $nem        = $_REQUEST['nem'];
 
 if(is_numeric($akim) && is_numeric($gerilim) && is_numeric($sicaklik) && is_numeric($nem)){
 
-    $db->query("INSERT INTO veriler(akim, gerilim, sicaklik, nem, tarih) 
-                VALUES('$akim','$gerilim','$sicaklik','$nem','".date('Y-m-d H:i:s')."')");
+    $db->query("INSERT INTO veriler(panel_id, akim, gerilim, sicaklik, nem, tarih)
+                VALUES('$panel','$akim','$gerilim','$sicaklik','$nem','".date('Y-m-d H:i:s')."')");
 
     echo ($db->insert_id > 0) ? '1' : 'err';
 
